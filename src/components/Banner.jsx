0920c8a8472b6cbfdb8af605e9ee6ae1f6834919
graceDev1 <grace.devolop@gmail.com>
2020-12-30 07:41:44 +0200

    routing

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,8 +2,11 @@ import React, { useState } from 'react'
 import '../style/Banner.css';
 import { Button } from '@material-ui/core';
 import Search from './Search';
+import { useHistory } from 'react-router-dom';
 
 function Banner() {
+    const history = useHistory();
+
     const [showSearch, setShowSearch] = useState(false);
     return (
         <div className="banner">
@@ -20,7 +23,9 @@ function Banner() {
             <div className="banner__info">
                 <h1>Get out and sketch your imagination</h1>
                 <h5>Plan a different kind of getaway to uncover the hidden gems near you</h5>
-                <Button variant="outlined">Explore Nearby</Button>
+                <Button variant="outlined"
+                onClick= {()=> history.push('/search')}
+                >Explore Nearby</Button>
             </div>
         </div>
     )
